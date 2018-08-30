01c1b54a5561a705cd92fbc3ff7d312b3e22f9f5
Jimmy Collazos <jimmy.collazos@gmail.com>
2018-08-30 12:18:35 +0200

    change integer property to array of string

--- currency.js
+++ currency.js
@@ -1,11 +1,13 @@
+const INT_SPLIT = /\d{3}/g
 const _isNegative = (x) => (x < 0)
 const _decimal = (x) => (x).toString().split('.')[1] || ''
 const _sign = (x) => ((x > 0) - (x < 0)) || +x // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
 const _integer = (x) => Math.floor(Math.abs(x)).toString()
+const _integerSplit = (x) => x.length > 3 && x.match(INT_SPLIT) || [x]
 
 module.exports = function Currency(x) {
   return {
-    integer: _integer(x),
+    integer: _integerSplit(_integer(x)),
     decimal: _decimal(x),
     isNegative: _isNegative(x),
     sign: _sign(x),
--- currency.test.js
+++ currency.test.js
@@ -1,22 +1,23 @@
 const Currency = require('./currency')
 
-const assert = (value, result, expected) => console.assert(result === expected, `${value} -> (${expected} === ${result})`)
+const _assert = (value, result, expected) => console.assert(result === expected, `${value} -> (${expected} === ${result})`)
+const assert = (value, result, expected) => _assert(JSON.stringify(value), JSON.stringify(result), JSON.stringify(expected))
 
 const tests = [
-  [ 1                , '1'        , ''       , false,  1],
-  [ 0                , '0'        , ''       , false,  0],
-  [-1                , '1'        , ''       , true , -1],
-  [ 0.1              , '0'        , '1'      , false,  1],
-  [ 0.0              , '0'        , ''       , false,  0],
-  [-0.1              , '0'        , '1'      , true , -1],
-  [ 123456789.9876543, '123456789', '9876543', false,  1],
-  [-123456789.9876543, '123456789', '9876543', true , -1],
-  [-987654321.1234567, '987654321', '1234567', true , -1]
+  [ 1                , ['1']        , ''       , false,  1],
+  [ 0                , ['0']        , ''       , false,  0],
+  [-1                , ['1']        , ''       , true , -1],
+  [ 0.1              , ['0']        , '1'      , false,  1],
+  [ 0.0              , ['0']        , ''       , false,  0],
+  [-0.1              , ['0']        , '1'      , true , -1],
+  [ 123456789.9876543, ['123','456','789'], '9876543', false,  1],
+  [-123456789.9876543, ['123','456','789'], '9876543', true , -1],
+  [-987654321.1234567, ['987','654','321'], '1234567', true , -1]
 ]
 
 tests.forEach(([assert_value, integer, decimal, isNegative, sign]) => {
   let currency = Currency(assert_value)
-  
+
   assert(assert_value, currency.integer, integer)
   assert(assert_value, currency.decimal, decimal, currency)
   assert(assert_value, currency.isNegative, isNegative)
